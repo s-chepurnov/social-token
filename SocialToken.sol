77742d9d94de2b643c1c36599119f4f2da05104f
s-chepurnov <chepurnov.serge@gmail.com>
2019-02-23 13:52:12 +0700

    merge 2 contracts

--- SocialToken.sol
+++ SocialToken.sol
@@ -9,8 +9,13 @@ import "github.com/s-chepurnov/social-token/owned.sol";
  * 
  * One Ether is 1e18 Wei
  * One Euro  is 1e18 EuroWei
+ *
+ * @dev Implementation of the basic standard token.
+ * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
+ * Originally based on code by FirstBlood: https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  */
-contract SocialToken is ERC20, owned {
+contract SocialToken is IERC20, owned {
+  using SafeMath for uint256;
 
   string public constant name = "SocialToken";
   string public constant symbol = "SDT";
@@ -26,17 +31,209 @@ contract SocialToken is ERC20, owned {
   uint256 public counterInvestment = 1; //uint8 possible
   mapping (address => bool) private registeredUsers;
 
+  //standart
+  mapping (address => uint256) private _balances;
+  mapping (address => mapping (address => uint256)) private _allowed;
+  uint256 private _totalSupply;
+
+
   /**
    * @dev Constructor that gives msg.sender all of existing tokens.
    */
-  //TODO: constructor should gives tokens for users.
-  constructor() public payable {
+  constructor() public {
      // airdrop
-     // create pre-addresses
      // 2 owners
     _mint(msg.sender, INITIAL_SUPPLY);
   }
 
+  /**
+  * @dev Total number of tokens in existence
+  */
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  * @param owner The address to query the balance of.
+  * @return An uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address owner) public view returns (uint256) {
+    return _balances[owner];
+  }
+
+  /**
+   * @dev Function to check the amount of tokens that an owner allowed to a spender.
+   * @param owner address The address which owns the funds.
+   * @param spender address The address which will spend the funds.
+   * @return A uint256 specifying the amount of tokens still available for the spender.
+   */
+  function allowance(
+    address owner,
+    address spender
+   )
+    public
+    view
+    returns (uint256)
+  {
+    return _allowed[owner][spender];
+  }
+
+  /**
+  * @dev Transfer token for a specified address
+  * @param to The address to transfer to.
+  * @param value The amount to be transferred.
+  */
+  function transfer(address to, uint256 value) public returns (bool) {
+    require(value <= _balances[msg.sender]);
+    require(to != address(0));
+
+    _balances[msg.sender] = _balances[msg.sender].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _trackTransfer(msg.sender, to, value);
+
+    emit Transfer(msg.sender, to, value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   * @param spender The address which will spend the funds.
+   * @param value The amount of tokens to be spent.
+   */
+  function approve(address spender, uint256 value) public returns (bool) {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = value;
+    emit Approval(msg.sender, spender, value);
+    return true;
+  }
+
+  /**
+   * @dev Transfer tokens from one address to another
+   * @param from address The address which you want to send tokens from
+   * @param to address The address which you want to transfer to
+   * @param value uint256 the amount of tokens to be transferred
+   */
+  function transferFrom(
+    address from,
+    address to,
+    uint256 value
+  )
+    public
+    returns (bool)
+  {
+    require(value <= _balances[from]);
+    require(value <= _allowed[from][msg.sender]);
+    require(to != address(0));
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+    _trackTransfer(from, to, value);
+
+    emit Transfer(from, to, value);
+    return true;
+  }
+
+  /**
+   * @dev Increase the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To increment
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param spender The address which will spend the funds.
+   * @param addedValue The amount of tokens to increase the allowance by.
+   */
+  function increaseAllowance(
+    address spender,
+    uint256 addedValue
+  )
+    public
+    returns (bool)
+  {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].add(addedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  /**
+   * @dev Decrease the amount of tokens that an owner allowed to a spender.
+   * approve should be called when allowed_[_spender] == 0. To decrement
+   * allowed value is better to use this function to avoid 2 calls (and wait until
+   * the first transaction is mined)
+   * From MonolithDAO Token.sol
+   * @param spender The address which will spend the funds.
+   * @param subtractedValue The amount of tokens to decrease the allowance by.
+   */
+  function decreaseAllowance(
+    address spender,
+    uint256 subtractedValue
+  )
+    public
+    returns (bool)
+  {
+    require(spender != address(0));
+
+    _allowed[msg.sender][spender] = (
+      _allowed[msg.sender][spender].sub(subtractedValue));
+    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+    return true;
+  }
+
+  /**
+   * @dev Internal function that mints an amount of the token and assigns it to
+   * an account. This encapsulates the modification of balances such that the
+   * proper events are emitted.
+   * @param account The account that will receive the created tokens.
+   * @param amount The amount that will be created.
+   */
+  function _mint(address account, uint256 amount) internal {
+    require(account != 0);
+    _totalSupply = _totalSupply.add(amount);
+    _balances[account] = _balances[account].add(amount);
+    emit Transfer(address(0), account, amount);
+  }
+
+  /**
+   * @dev Internal function that burns an amount of the token of a given
+   * account.
+   * @param account The account whose tokens will be burnt.
+   * @param amount The amount that will be burnt.
+   */
+  function _burn(address account, uint256 amount) internal {
+    require(account != 0);
+    require(amount <= _balances[account]);
+
+    _totalSupply = _totalSupply.sub(amount);
+    _balances[account] = _balances[account].sub(amount);
+    emit Transfer(account, address(0), amount);
+  }
+
+  /**
+   * @dev Internal function that burns an amount of the token of a given
+   * account, deducting from the sender's allowance for said account. Uses the
+   * internal burn function.
+   * @param account The account whose tokens will be burnt.
+   * @param amount The amount that will be burnt.
+   */
+  function _burnFrom(address account, uint256 amount) internal {
+    require(amount <= _allowed[account][msg.sender]);
+
+    // Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
+    // this function needs to emit an event with the updated approval.
+    _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(
+      amount);
+    _burn(account, amount);
+  }
+  
   event TrackTransfer(
     address indexed from,	  
     address indexed to,
@@ -78,12 +275,12 @@ contract SocialToken is ERC20, owned {
     require (_balances[_to] + _value >= _balances[_to]);    // Check for overflows
     _balances[_from] = _balances[_from].sub(_value);        // Subtract from the sender
     _balances[_to] = _balances[_to].add(_value);           // Add the same to the recipient
+
+    _trackTransfer(_from, _to, _value);
     emit Transfer(_from, _to, _value);
   }
 
   function _trackTransfer(address _from, address _to, uint256 _amount) internal {
-    _transfer(_from, _to, _amount);
-
     // increase price after every 1 000 000 Euro transfered value
     // 1 000 000 Euro = 1e24 EuroWei
     transferedValue = transferedValue.add(_amount);
@@ -103,7 +300,7 @@ contract SocialToken is ERC20, owned {
     }
 
     uint256 amount = 10000;//tokens
-    _trackTransfer(address(this), _newUserAddress, amount);
+    _transfer(msg.sender, _newUserAddress, amount);
 
     userCounter = userCounter.add(1);
     //increase price by every new user
@@ -132,7 +329,7 @@ contract SocialToken is ERC20, owned {
     amount = msg.value/priceOfOneTokenInWei;
     //makes the transfers of tokens
     //_transfer(address(this), msg.sender, amount);
-    _trackTransfer(address(this), msg.sender, amount);
+    _transfer(address(this), msg.sender, amount);
 
     emit Buy(amount, priceOfOneTokenInWei, msg.sender);
     return amount;
@@ -148,7 +345,7 @@ contract SocialToken is ERC20, owned {
     //'msg.sender.send' means the contract sends Ether to 'msg.sender'
     require(msg.sender.send(revenue));   
     //makes the transfers of tokens
-    _trackTransfer(msg.sender, address(this), _amount);
+    _transfer(msg.sender, address(this), _amount);
 
     emit Sell(_amount, revenue, priceOfOneTokenInWei, msg.sender);
     return revenue;
